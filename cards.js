39d00bd0b5e3f53096283d68d2492cc7c7991e77
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-03-28 09:26:35 +0100

    create cards file

---
+++ cards.js
@@ -0,0 +1,258 @@
+export const cards = [
+    {
+        src: '',
+        value: 1
+    },
+    {
+        src: '',
+        value: 1
+    },
+    {
+        src: '',
+        value: 2
+    },
+    {
+        src: '',
+        value: 2
+    },
+    {
+        src: '',
+        value: 3
+    },
+    {
+        src: '',
+        value: 3
+    },
+    {
+        src: '',
+        value: 4
+    },
+    {
+        src: '',
+        value: 4
+    },
+    {
+        src: '',
+        value: 5
+    },
+    {
+        src: '',
+        value: 5
+    },
+    {
+        src: '',
+        value: 6
+    },
+    {
+        src: '',
+        value: 6
+    },
+    {
+        src: '',
+        value: 7
+    },
+    {
+        src: '',
+        value: 7
+    },
+    {
+        src: '',
+        value: 8
+    },
+    {
+        src: '',
+        value: 8
+    },
+    {
+        src: '',
+        value: 9
+    },
+    {
+        src: '',
+        value: 9
+    },
+    {
+        src: '',
+        value: 10
+    },
+    {
+        src: '',
+        value: 10
+    },
+    {
+        src: '',
+        value: 11
+    },
+    {
+        src: '',
+        value: 11
+    },
+    {
+        src: '',
+        value: 12
+    },
+    {
+        src: '',
+        value: 12
+    },
+    {
+        src: '',
+        value: 13
+    },
+    {
+        src: '',
+        value: 13
+    },
+    {
+        src: '',
+        value: 14
+    },
+    {
+        src: '',
+        value: 14
+    },
+    {
+        src: '',
+        value: 15
+    },
+    {
+        src: '',
+        value: 15
+    },
+    {
+        src: '',
+        value: 16
+    },
+    {
+        src: '',
+        value: 16
+    },
+    {
+        src: '',
+        value: 17
+    },
+    {
+        src: '',
+        value: 17
+    },
+    {
+        src: '',
+        value: 18
+    },
+    {
+        src: '',
+        value: 18
+    },
+    {
+        src: '',
+        value: 19
+    },
+    {
+        src: '',
+        value: 19
+    },
+    {
+        src: '',
+        value: 20
+    },
+    {
+        src: '',
+        value: 20
+    },
+    {
+        src: '',
+        value: 21
+    },
+    {
+        src: '',
+        value: 21
+    },
+    {
+        src: '',
+        value: 22
+    },
+    {
+        src: '',
+        value: 22
+    },
+    {
+        src: '',
+        value: 23
+    },
+    {
+        src: '',
+        value: 23
+    },
+    {
+        src: '',
+        value: 24
+    },
+    {
+        src: '',
+        value: 24
+    },
+    {
+        src: '',
+        value: 25
+    },
+    {
+        src: '',
+        value: 25
+    },
+    {
+        src: '',
+        value: 26
+    },
+    {
+        src: '',
+        value: 26
+    },
+    {
+        src: '',
+        value: 27
+    },
+    {
+        src: '',
+        value: 27
+    },
+    {
+        src: '',
+        value: 28
+    },
+    {
+        src: '',
+        value: 28
+    },
+    {
+        src: '',
+        value: 29
+    },
+    {
+        src: '',
+        value: 29
+    },
+    {
+        src: '',
+        value: 30
+    },
+    {
+        src: '',
+        value: 30
+    },
+    {
+        src: '',
+        value: 31
+    },
+    {
+        src: '',
+        value: 31
+    },
+    {
+        src: '',
+        value: 32
+    },
+    {
+        src: '',
+        value: 32
+    },
+]
