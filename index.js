8a8424eb78ee884c66c164d27ce4e3faff3fa3fc
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-03-28 12:12:16 +0100

    apply difficulty change and board initialization

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const difUp = document.getElementById('difficulty-up');
 const difficulty = document.getElementById('difficulty-level');
 const light = document.getElementById('light-mode');
 const sound = document.getElementById('sound-mode');
+const board = document.getElementById('game');
 const root = document.documentElement;
+
+let isGameInProgress = false;
+
+//Toggle themes:
 
 light.addEventListener('click', () => {
     root.classList.toggle('light');
@@ -14,3 +19,53 @@ light.addEventListener('click', () => {
     light.classList.toggle('fa-moon');
     light.classList.toggle('fa-sun');
 });
+
+//Change difficulty:
+let difIndex = 0;
+let difficultyLevel = difficultyLevels[difIndex];
+difficulty.innerText = difficultyLevel.level;
+
+difUp.addEventListener('click', () => {
+    if (difIndex !== 2) {
+        difIndex++;
+        difficultyLevel = difficultyLevels[difIndex];
+        difficulty.innerText = difficultyLevel.level;
+
+    }
+    if (!isGameInProgress) {
+        initializeBoard();
+    }
+});
+difDown.addEventListener('click', () => {
+    if (difIndex !== 0) {
+        difIndex--;
+        difficultyLevel = difficultyLevels[difIndex];
+        difficulty.innerText = difficultyLevel.level;
+
+    }
+    if (!isGameInProgress) {
+        initializeBoard();
+    }
+
+});
+
+//Initialize game board:
+
+function initializeBoard() {
+    board.innerHTML = ''
+    let size = difficultyLevel.tiles;
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++) {
+        let tile = document.createElement('div');
+        tile.className = 'card-hidden';
+        tile.addEventListener('click', ()=> {
+            tile.classList.toggle('card-hidden');
+            tile.classList.toggle('card-open');
+        })
+        board.appendChild(tile);
+    }
+}
+
+initializeBoard()
+
