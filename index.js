a8e9b0611fc9d5013970966cbee51647572c0e6e
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-03-28 11:18:04 +0100

    apply theme switch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { cards, difficultyLevels } from './misc'
+import { cards, difficultyLevels } from './misc.js'
 
 const newGame = document.getElementById('new-game');
 const difDown = document.getElementById('difficulty-down');
@@ -6,3 +6,11 @@ const difUp = document.getElementById('difficulty-up');
 const difficulty = document.getElementById('difficulty-level');
 const light = document.getElementById('light-mode');
 const sound = document.getElementById('sound-mode');
+const root = document.documentElement;
+
+light.addEventListener('click', () => {
+    root.classList.toggle('light');
+    root.classList.toggle('dark');
+    light.classList.toggle('fa-moon');
+    light.classList.toggle('fa-sun');
+});
