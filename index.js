ad5863bc0662d1da7abc4eac97fb6207685df9ef
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-03-29 10:16:48 +0100

    matching cards work

--- index.js
+++ index.js
@@ -51,46 +51,106 @@ difDown.addEventListener('click', () => {
 
 //Initialize game board and handle card clicks:
 
-let openCards = 0;
+let firstCardOpen = false;
+let secondCardOpen = false;
+let previousCardValue;
 
 function initializeBoard() {
     board.innerHTML = ''
     let size = difficultyLevel.tiles;
     let tilesArray = cards.slice(0, size * size).sort((a, b) => 0.5 - Math.random());
-    console.log(tilesArray);
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for (let i = 0; i < size * size; i++) {
         let tile = document.createElement('div');
         tile.className = 'card-hidden';
         tile.indexPosition = i;
-        tile.addEventListener('click', () => {
-            if (tile.className === 'card-hidden' && openCards < 2) {
-                tile.classList.remove('card-hidden');
-                tile.classList.add('card-open');
-                tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`
-                openCards++
-            } else if (tile.className === 'card-hidden' && openCards == 2) {
-                openCards = 0;
-                let cardsOpen = document.querySelectorAll('.card-open');
-                cardsOpen.forEach(card => {
-                    card.classList.remove('card-open');
-                    card.classList.add('card-hidden');
-                    card.style.background = 'linear-gradient(120deg, var(--middle), var(--dark))'
-                });
-                tile.classList.remove('card-hidden');
-                tile.classList.add('card-open');
-                tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`
-                openCards ++
+        // tile.isSolved = false;
+        // tile.addEventListener('click', () => {
+        //     if (tile.className === 'card-hidden' && openCardsNum < 2) {
+        //         tile.classList.remove('card-hidden');
+        //         tile.classList.add('card-open');
+        //         tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`
+        //         if (openCardsNum === 0) {
+        //             openCardValue = tilesArray[tile.indexPosition].value
+        //         } else if (openCardsNum ===1) {if(openCardValue == tilesArray[tile.indexPosition].value) {
+        //             let cardsOpen = document.querySelectorAll('.card-open');
+        //             cardsOpen.forEach(card => {
+        //                 card.classList.remove('card-open');
+        //                 card.classList.add('card-solved');
+        //                 card.style.border = '3px solid gold';
+        //                 card.style.borderRadius = '15%';
+        //             });
+        //             openCardsNum = 0;} 
 
-                // tile.classList.remove('card-open');
-                // tile.classList.add('card-hidden');
-                // tile.style.background = 'linear-gradient(120deg, var(--middle), var(--dark))'
+        //         }
+
+        //         openCardsNum++
+        //     } else if (tile.className === 'card-hidden' && openCardsNum == 2) {
+        //         openCardsNum = 0;
+        //         let cardsOpen = document.querySelectorAll('.card-open');
+        //         cardsOpen.forEach(card => {
+        //             card.classList.remove('card-open');
+        //             card.classList.add('card-hidden');
+        //             card.style.background = 'linear-gradient(120deg, var(--middle), var(--dark))'
+        //         });
+        //         tile.classList.remove('card-hidden');
+        //         tile.classList.add('card-open');
+        //         tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`
+        //         openCardsNum++
+
+        //         // tile.classList.remove('card-open');
+        //         // tile.classList.add('card-hidden');
+        //         // tile.style.background = 'linear-gradient(120deg, var(--middle), var(--dark))'
+        //     }
+        // })
+
+        tile.addEventListener('click', () => {
+            if (tile.className === 'card-hidden') {
+                if (!firstCardOpen) {
+                    previousCardValue = tilesArray[tile.indexPosition].value;
+                    console.log(firstCardOpen, secondCardOpen, '1', previousCardValue)
+                    tile.classList.remove('card-hidden');
+                    tile.classList.add('card-open');
+                    tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`;
+
+                    firstCardOpen = true;
+                } else if (firstCardOpen && !secondCardOpen) {
+                    console.log(firstCardOpen, secondCardOpen, '2', previousCardValue)
+                    tile.classList.remove('card-hidden');
+                    tile.classList.add('card-open');
+                    tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`;
+                    secondCardOpen = true;
+                    if (previousCardValue === tilesArray[tile.indexPosition].value) {
+                        let cardsOpen = document.querySelectorAll('.card-open');
+                        cardsOpen.forEach(card => {
+                            card.classList.remove('card-open');
+                            card.classList.add('card-solved');
+                            card.style.border = '3px solid gold';
+                            card.style.borderRadius = '15%';
+                        });
+                    }
+                } else if (firstCardOpen && secondCardOpen) {
+                    previousCardValue = tilesArray[tile.indexPosition].value;
+                    console.log(firstCardOpen, secondCardOpen, '3', previousCardValue)
+                    let cardsOpen = document.querySelectorAll('.card-open');
+                    cardsOpen.forEach(card => {
+                        card.classList.remove('card-open');
+                        card.classList.add('card-hidden');
+                        card.style.background = 'linear-gradient(120deg, var(--middle), var(--dark))'
+                    });
+                    tile.classList.remove('card-hidden');
+                    tile.classList.add('card-open');
+                    tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`;
+                    secondCardOpen = false;
+
+                }
             }
         })
         board.appendChild(tile);
     }
 }
 
+newGame.addEventListener('click', initializeBoard);
 initializeBoard()
 
