128df46e947ff97116ce17cf8b8267d6c76f574f
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-03-29 10:52:10 +0100

    count matches and stop game when all matches are found

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const light = document.getElementById('light-mode');
 const sound = document.getElementById('sound-mode');
 const board = document.getElementById('game');
 const root = document.documentElement;
+const timer = document.getElementById('timer');
 
 let isGameInProgress = false;
 
@@ -51,12 +52,13 @@ difDown.addEventListener('click', () => {
 
 //Initialize game board and handle card clicks:
 
-let firstCardOpen = false;
-let secondCardOpen = false;
-let previousCardValue;
 
 function initializeBoard() {
-    board.innerHTML = ''
+    board.innerHTML = '';
+    let firstCardOpen = false;
+    let secondCardOpen = false;
+    let previousCardValue = 0;
+    let solvedPairs = 0;
     let size = difficultyLevel.tiles;
     let tilesArray = cards.slice(0, size * size).sort((a, b) => 0.5 - Math.random());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -65,58 +67,17 @@ function initializeBoard() {
         let tile = document.createElement('div');
         tile.className = 'card-hidden';
         tile.indexPosition = i;
-        // tile.isSolved = false;
-        // tile.addEventListener('click', () => {
-        //     if (tile.className === 'card-hidden' && openCardsNum < 2) {
-        //         tile.classList.remove('card-hidden');
-        //         tile.classList.add('card-open');
-        //         tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`
-        //         if (openCardsNum === 0) {
-        //             openCardValue = tilesArray[tile.indexPosition].value
-        //         } else if (openCardsNum ===1) {if(openCardValue == tilesArray[tile.indexPosition].value) {
-        //             let cardsOpen = document.querySelectorAll('.card-open');
-        //             cardsOpen.forEach(card => {
-        //                 card.classList.remove('card-open');
-        //                 card.classList.add('card-solved');
-        //                 card.style.border = '3px solid gold';
-        //                 card.style.borderRadius = '15%';
-        //             });
-        //             openCardsNum = 0;} 
-
-        //         }
-
-        //         openCardsNum++
-        //     } else if (tile.className === 'card-hidden' && openCardsNum == 2) {
-        //         openCardsNum = 0;
-        //         let cardsOpen = document.querySelectorAll('.card-open');
-        //         cardsOpen.forEach(card => {
-        //             card.classList.remove('card-open');
-        //             card.classList.add('card-hidden');
-        //             card.style.background = 'linear-gradient(120deg, var(--middle), var(--dark))'
-        //         });
-        //         tile.classList.remove('card-hidden');
-        //         tile.classList.add('card-open');
-        //         tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`
-        //         openCardsNum++
-
-        //         // tile.classList.remove('card-open');
-        //         // tile.classList.add('card-hidden');
-        //         // tile.style.background = 'linear-gradient(120deg, var(--middle), var(--dark))'
-        //     }
-        // })
-
         tile.addEventListener('click', () => {
             if (tile.className === 'card-hidden') {
                 if (!firstCardOpen) {
+                    solvedPairs = 0;
+                    isGameInProgress = true;
                     previousCardValue = tilesArray[tile.indexPosition].value;
-                    console.log(firstCardOpen, secondCardOpen, '1', previousCardValue)
                     tile.classList.remove('card-hidden');
                     tile.classList.add('card-open');
                     tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`;
-
                     firstCardOpen = true;
                 } else if (firstCardOpen && !secondCardOpen) {
-                    console.log(firstCardOpen, secondCardOpen, '2', previousCardValue)
                     tile.classList.remove('card-hidden');
                     tile.classList.add('card-open');
                     tile.style.background = `center/contain no-repeat url('${tilesArray[tile.indexPosition].src}')`;
@@ -129,10 +90,15 @@ function initializeBoard() {
                             card.style.border = '3px solid gold';
                             card.style.borderRadius = '15%';
                         });
+                        solvedPairs++
+                        console.log(solvedPairs)
+                        if (solvedPairs === size*size/2) {
+                            isGameInProgress = false;
+                            console.log('You win')
+                        }
                     }
                 } else if (firstCardOpen && secondCardOpen) {
                     previousCardValue = tilesArray[tile.indexPosition].value;
-                    console.log(firstCardOpen, secondCardOpen, '3', previousCardValue)
                     let cardsOpen = document.querySelectorAll('.card-open');
                     cardsOpen.forEach(card => {
                         card.classList.remove('card-open');
@@ -151,6 +117,8 @@ function initializeBoard() {
     }
 }
 
-newGame.addEventListener('click', initializeBoard);
-initializeBoard()
+// Game on
 
+newGame.addEventListener('click', initializeBoard);
+
+initializeBoard();
--- misc.js
+++ misc.js
@@ -1,10 +1,10 @@
 const cards = [
     {
-        src: './images/rsz_2deer.png',
+        src: './images/rsz_shrimp.png',
         value: 1
     },
     {
-        src: './images/rsz_2deer.png',
+        src: './images/rsz_shrimp.png',
         value: 1
     },
     {
@@ -22,221 +22,221 @@ const cards = [
     {
         src: './images/rsz_avocado.png',
         value: 3
-    },
-    {
-        src: './images/rsz_beaver.png',
-        value: 4
-    },
-    {
-        src: './images/rsz_beaver.png',
-        value: 4
-    },
-    {
-        src: './images/rsz_burger.png',
-        value: 5
-    },
-    {
-        src: './images/rsz_burger.png',
-        value: 5
-    },
-    {
-        src: './images/rsz_burrito.png',
-        value: 6
-    },
-    {
-        src: './images/rsz_burrito.png',
-        value: 6
-    },
-    {
-        src: './images/rsz_cake.png',
-        value: 7
-    },
-    {
-        src: './images/rsz_cake.png',
-        value: 7
-    },
-    {
-        src: './images/rsz_cat.png',
-        value: 8
-    },
-    {
-        src: './images/rsz_cat.png',
-        value: 8
-    },
-    {
-        src: './images/rsz_chicken.png',
-        value: 9
-    },
-    {
-        src: './images/rsz_chicken.png',
-        value: 9
-    },
-    {
-        src: './images/rsz_chocolate.png',
-        value: 10
-    },
-    {
-        src: './images/rsz_chocolate.png',
-        value: 10
-    },
-    {
-        src: './images/rsz_cow.png',
-        value: 11
-    },
-    {
-        src: './images/rsz_cow.png',
-        value: 11
-    },
-    {
-        src: './images/rsz_cupcake.png',
-        value: 12
-    },
-    {
-        src: './images/rsz_cupcake.png',
-        value: 12
-    },
-    {
-        src: './images/rsz_dog.png',
-        value: 13
-    },
-    {
-        src: './images/rsz_dog.png',
-        value: 13
-    },
-    {
-        src: './images/rsz_donut.png',
-        value: 14
-    },
-    {
-        src: './images/rsz_donut.png',
-        value: 14
-    },
-    {
-        src: './images/rsz_elephant.png',
-        value: 15
-    },
-    {
-        src: './images/rsz_elephant.png',
-        value: 15
-    },
-    {
-        src: './images/rsz_fox.png',
-        value: 16
-    },
-    {
-        src: './images/rsz_fox.png',
-        value: 16
-    },
-    {
-        src: './images/rsz_fries.png',
-        value: 17
-    },
-    {
-        src: './images/rsz_fries.png',
-        value: 17
-    },
-    {
-        src: './images/rsz_lollipop.png',
-        value: 18
-    },
-    {
-        src: './images/rsz_lollipop.png',
-        value: 18
-    },
-    {
-        src: './images/rsz_monkey.png',
-        value: 19
-    },
-    {
-        src: './images/rsz_monkey.png',
-        value: 19
-    },
-    {
-        src: './images/rsz_panda.png',
-        value: 20
-    },
-    {
-        src: './images/rsz_panda.png',
-        value: 20
-    },
-    {
-        src: './images/rsz_pig.png',
-        value: 21
-    },
-    {
-        src: './images/rsz_pig.png',
-        value: 21
-    },
-    {
-        src: './images/rsz_pizza.png',
-        value: 22
-    },
-    {
-        src: './images/rsz_pizza.png',
-        value: 22
-    },
-    {
-        src: './images/rsz_sheep.png',
-        value: 23
-    },
-    {
-        src: './images/rsz_sheep.png',
-        value: 23
-    },
-    {
-        src: './images/rsz_shrimp.png',
-        value: 24
-    },
-    {
-        src: './images/rsz_shrimp.png',
-        value: 24
-    },
-    {
-        src: './images/rsz_sushi1.png',
-        value: 25
-    },
-    {
-        src: './images/rsz_sushi1.png',
-        value: 25
-    },
-    {
-        src: './images/rsz_sushi2.png',
-        value: 26
-    },
-    {
-        src: './images/rsz_sushi2.png',
-        value: 26
-    },
-    {
-        src: './images/rsz_sushi3.png',
-        value: 27
-    },
-    {
-        src: './images/rsz_sushi3.png',
-        value: 27
-    },
-    {
-        src: './images/rsz_sushi4.png',
-        value: 28
-    },
-    {
-        src: './images/rsz_sushi4.png',
-        value: 28
-    },
-    {
-        src: './images/rsz_sushi5.png',
-        value: 29
-    },
-    {
-        src: './images/rsz_sushi5.png',
-        value: 29
     },
     {
         src: './images/rsz_sushi6.png',
+        value: 4
+    },
+    {
+        src: './images/rsz_sushi6.png',
+        value: 4
+    },
+    {
+        src: './images/rsz_burger.png',
+        value: 5
+    },
+    {
+        src: './images/rsz_burger.png',
+        value: 5
+    },
+    {
+        src: './images/rsz_burrito.png',
+        value: 6
+    },
+    {
+        src: './images/rsz_burrito.png',
+        value: 6
+    },
+    {
+        src: './images/rsz_cake.png',
+        value: 7
+    },
+    {
+        src: './images/rsz_cake.png',
+        value: 7
+    },
+    {
+        src: './images/rsz_pizza.png',
+        value: 8
+    },
+    {
+        src: './images/rsz_pizza.png',
+        value: 8
+    },
+    {
+        src: './images/rsz_chicken.png',
+        value: 9
+    },
+    {
+        src: './images/rsz_chicken.png',
+        value: 9
+    },
+    {
+        src: './images/rsz_chocolate.png',
+        value: 10
+    },
+    {
+        src: './images/rsz_chocolate.png',
+        value: 10
+    },
+    {
+        src: './images/rsz_cow.png',
+        value: 11
+    },
+    {
+        src: './images/rsz_cow.png',
+        value: 11
+    },
+    {
+        src: './images/rsz_cupcake.png',
+        value: 12
+    },
+    {
+        src: './images/rsz_cupcake.png',
+        value: 12
+    },
+    {
+        src: './images/rsz_dog.png',
+        value: 13
+    },
+    {
+        src: './images/rsz_dog.png',
+        value: 13
+    },
+    {
+        src: './images/rsz_donut.png',
+        value: 14
+    },
+    {
+        src: './images/rsz_donut.png',
+        value: 14
+    },
+    {
+        src: './images/rsz_elephant.png',
+        value: 15
+    },
+    {
+        src: './images/rsz_elephant.png',
+        value: 15
+    },
+    {
+        src: './images/rsz_fox.png',
+        value: 16
+    },
+    {
+        src: './images/rsz_fox.png',
+        value: 16
+    },
+    {
+        src: './images/rsz_fries.png',
+        value: 17
+    },
+    {
+        src: './images/rsz_fries.png',
+        value: 17
+    },
+    {
+        src: './images/rsz_lollipop.png',
+        value: 18
+    },
+    {
+        src: './images/rsz_lollipop.png',
+        value: 18
+    },
+    {
+        src: './images/rsz_monkey.png',
+        value: 19
+    },
+    {
+        src: './images/rsz_monkey.png',
+        value: 19
+    },
+    {
+        src: './images/rsz_panda.png',
+        value: 20
+    },
+    {
+        src: './images/rsz_panda.png',
+        value: 20
+    },
+    {
+        src: './images/rsz_pig.png',
+        value: 21
+    },
+    {
+        src: './images/rsz_pig.png',
+        value: 21
+    },
+    {
+        src: './images/rsz_cat.png',
+        value: 22
+    },
+    {
+        src: './images/rsz_cat.png',
+        value: 22
+    },
+    {
+        src: './images/rsz_sheep.png',
+        value: 23
+    },
+    {
+        src: './images/rsz_sheep.png',
+        value: 23
+    },
+    {
+        src: './images/rsz_2deer.png',
+        value: 24
+    },
+    {
+        src: './images/rsz_2deer.png',
+        value: 24
+    },
+    {
+        src: './images/rsz_sushi1.png',
+        value: 25
+    },
+    {
+        src: './images/rsz_sushi1.png',
+        value: 25
+    },
+    {
+        src: './images/rsz_sushi2.png',
+        value: 26
+    },
+    {
+        src: './images/rsz_sushi2.png',
+        value: 26
+    },
+    {
+        src: './images/rsz_sushi3.png',
+        value: 27
+    },
+    {
+        src: './images/rsz_sushi3.png',
+        value: 27
+    },
+    {
+        src: './images/rsz_sushi4.png',
+        value: 28
+    },
+    {
+        src: './images/rsz_sushi4.png',
+        value: 28
+    },
+    {
+        src: './images/rsz_sushi5.png',
+        value: 29
+    },
+    {
+        src: './images/rsz_sushi5.png',
+        value: 29
+    },
+    {
+        src: './images/rsz_beaver.png',
         value: 30
     },
     {
-        src: './images/rsz_sushi6.png',
+        src: './images/rsz_beaver.png',
         value: 30
     },
     {
